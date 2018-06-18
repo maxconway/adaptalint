9bde051667a313f32630147ecabf36bf807147d7
maxconway <conway.max1@gmail.com>
2018-06-18 15:42:39 +0100

    First commit

---
+++ R/styles.R
@@ -0,0 +1,31 @@
+#' Extract lint error distribution from a package
+#'
+#' Returns the absolute and relative counts of all lint errors found in the package.
+#'
+#' @param package path to the package of interest
+#'
+#' @return a data_frame with columns \code{lint}, \code{count}, \code{total_lints} and \code{adjusted}
+#'
+#' @import lintr
+#' @import dplyr
+#'
+#' @export
+extract_style <- function(package){
+  package %>%
+    lint_package() %>%
+    as_tibble() %>%
+    mutate(total_lints = n()) %>%
+    group_by(linter) %>%
+    summarise(count = n(),
+              total_lints = mean(total_lints)
+              ) %>%
+    mutate(adjusted = count/total_lints)
+}
+
+check_with_style <- function(package, style, threshold = 0.01){
+  to_check <- style %>%
+    filter(adjusted < threshold) %>%
+    `$`('linter')
+
+  lint_package(package, linters = to_check %>% map(get))
+}
