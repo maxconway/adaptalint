c742bf9733cfb910b6990868d7df2d7681915585
maxconway <conway.max1@gmail.com>
2018-06-19 10:48:00 +0100

    Improve docs

--- R/styles.R
+++ R/styles.R
@@ -1,6 +1,9 @@
-#' Extract lint error distribution from a package
+#' Find style of a package
 #'
-#' Returns the absolute and relative counts of all lint errors found in the package.
+#' Returns the absolute and relative counts of all lint errors found in the
+#' package.
+#' The resulting style data can be applied to another file with
+#' \code{\link{check_with_style}}.
 #'
 #' @param package path to the package of interest
 #'
@@ -22,6 +25,13 @@ extract_style <- function(package){
     mutate(adjusted = count/total_lints)
 }
 
+#' Lint a package, using the style of another package
+#'
+#' Apply a style extracted using \code{\link{extract_style}}, in order to
+#' check for only the style issues that aren't excepted in that package.
+#'
+#' @import purrr
+#' @import lintr
 check_with_style <- function(package, style, threshold = 0.01){
   to_ignore <- style %>%
     filter(adjusted > threshold) %>%
