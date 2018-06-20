c7345db5193c321e8043d3a0ba2f1843b592f392
maxconway <conway.max1@gmail.com>
2018-06-20 15:55:04 +0100

    test fixes

--- tests/testthat/test-basics.R
+++ tests/testthat/test-basics.R
@@ -1,20 +1,22 @@
 context("basics")
 
+adaptalint_path <- '.'
+
 test_that("exports are correct", {
   expect_equal(find('extract_style'), 'package:adaptalint')
   expect_equal(find('lint_with_style'), 'package:adaptalint')
 })
 
 test_that("extract works in simple case", {
-  a <- extract_style(find.package('adaptalint'))
+  a <- extract_style(adaptalint_path)
   expect_is(a, 'data.frame')
 })
 
 test_that("apply works in simple case", {
   data("style_dplyr")
 
-  a <- lintr::lint_package(find.package('adaptalint'))
-  b <- lint_with_style('.', style_dplyr)
+  a <- lintr::lint_package(adaptalint_path)
+  b <- lint_with_style(adaptalint_path, style_dplyr)
   expect_is(a, 'lints')
   expect_is(b, 'lints')
   expect_lte(length(b), length(a))
